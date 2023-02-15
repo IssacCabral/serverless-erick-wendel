9691546d367f6340248489625f33483d14a47301
Issac Cabral <issaccabrall@gmail.com>
2023-02-15 15:36:27 -0300

    refactor: rename folder

---
+++ demo02-lambda-no-frameworks/run.sh
@@ -0,0 +1,9 @@
+# 1° criar arquivos de políticas de segurança (falar o que a nossa lambda pode fazer e o que ela pode acessar)
+
+# 2° attachar a role para a nossa lambda
+
+ROLE_NAME=lambda-example
+NODEJS_VERSION=nodejs18.x
+FUNCTION_NAME=hello-cli
+
+mkdir -p logs
