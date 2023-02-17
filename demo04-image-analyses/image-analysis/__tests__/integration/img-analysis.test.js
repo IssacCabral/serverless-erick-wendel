3fddced2f23785f82de157b382b49580358f253c
Issac Cabral <issaccabrall@gmail.com>
2023-02-17 11:58:10 -0300

    fix: resolve todo

--- demo04-image-analyses/image-analysis/__tests__/integration/img-analysis.test.js
+++ demo04-image-analyses/image-analysis/__tests__/integration/img-analysis.test.js
@@ -1,15 +1,9 @@
 const { describe, it, expect } = require("@jest/globals");
 
 describe("Image Analyser test suit", () => {
-  it.todo(
-    "it should analyse successfuly the image returning the results",
-    () => {}
-  );
+  test.todo("it should analyse successfuly the image returning the results");
 
-  it.todo(
-    "given an empty queryString it should return status code 400",
-    () => {}
-  );
+  test.todo("given an empty queryString it should return status code 400");
 
-  it.todo("given an invalid ImageURL it should return 500", () => {});
+  test.todo("given an invalid ImageURL it should return 500");
 });
