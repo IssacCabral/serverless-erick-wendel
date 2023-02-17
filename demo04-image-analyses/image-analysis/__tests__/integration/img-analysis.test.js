42423eda6695e26e8fe2ffa3aef0f81a76888f01
Issac Cabral <issaccabrall@gmail.com>
2023-02-17 11:49:45 -0300

    chore: add jest

---
+++ demo04-image-analyses/image-analysis/__tests__/integration/img-analysis.test.js
@@ -0,0 +1,15 @@
+const { describe, it, expect } = require("@jest/globals");
+
+describe("Image Analyser test suit", () => {
+  it.todo(
+    "it should analyse successfuly the image returning the results",
+    () => {}
+  );
+
+  it.todo(
+    "given an empty queryString it should return status code 400",
+    () => {}
+  );
+
+  it.todo("given an invalid ImageURL it should return 500", () => {});
+});
