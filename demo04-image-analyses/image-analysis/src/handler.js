effba65949bb41f08cee064e7c53c96419813ab1
Issac Cabral <issaccabrall@gmail.com>
2023-02-18 15:22:12 -0300

    feat: analyze an image and translate the result

--- demo04-image-analyses/image-analysis/src/handler.js
+++ demo04-image-analyses/image-analysis/src/handler.js
@@ -1,16 +1,84 @@
+const { get } = require("axios");
+
 class Handler {
   constructor({ rekoSvc, translatorSvc }) {
     this.rekoSvc = rekoSvc;
     this.translatorSvc = translatorSvc;
   }
 
-  async main(event) {
-    console.log(event);
+  async getImageBuffer(imageUrl) {
+    const response = await get(imageUrl, {
+      responseType: "arraybuffer",
+    });
+    const buffer = Buffer.from(response.data, "base64");
+    return buffer;
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
+
+    const names = workingItems.map(({ Name }) => Name);
 
     return {
-      statusCode: 200,
-      body: "Hello",
+      names,
+      workingItems,
     };
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text.toString(),
+    };
+
+    const { TranslatedText } = await this.translatorSvc
+      .translateText(params)
+      .promise();
+
+    return TranslatedText.split(",");
+  }
+
+  async main(event) {
+    try {
+      const { imageUrl } = event.queryStringParameters;
+
+      if (!imageUrl) {
+        return {
+          statusCode: 400,
+          body: "an IMG URL is required",
+        };
+      }
+
+      console.log("downloading image...");
+      const buffer = await this.getImageBuffer(imageUrl);
+
+      console.log("detecting labels...");
+      const { names, workingItems } = await this.detectImageLabels(buffer);
+
+      console.log("translating to portuguese...");
+      const texts = await this.translateText(names);
+      console.log(texts);
+      return {
+        statusCode: 200,
+        body: "Hello",
+      };
+    } catch (err) {
+      console.error(err.stack);
+      return {
+        statusCode: 500,
+        body: "Internal Server Error",
+      };
+    }
   }
 }
 
