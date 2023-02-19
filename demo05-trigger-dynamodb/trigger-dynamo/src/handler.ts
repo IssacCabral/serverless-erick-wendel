61a4a3f695c6713e6d78634c9f614ebff473946b
Issac Cabral <issaccabrall@gmail.com>
2023-02-19 16:29:35 -0300

    feat: insert item on dynamodb

--- demo05-trigger-dynamodb/trigger-dynamo/src/handler.ts
+++ demo05-trigger-dynamodb/trigger-dynamo/src/handler.ts
@@ -1,20 +1,54 @@
+import * as AWS from "aws-sdk";
 import { APIGatewayEvent } from "aws-lambda";
+import { v1 } from "uuid";
 
 class Handler {
-  constructor(private readonly dynamoDbSvc) {}
+  private readonly dynamoDbTable: string;
+  constructor(private readonly dynamoDbSvc: AWS.DynamoDB.DocumentClient) {
+    this.dynamoDbTable = process.env.DYNAMODB_TABLE ?? "";
+  }
+
+  private prepareData(data) {
+    const params = {
+      TableName: this.dynamoDbTable,
+      Item: {
+        ...data,
+        id: v1(),
+        createdAt: new Date().toISOString(),
+      },
+    };
+    return params;
+  }
+
+  private async insertItem(params) {
+    return this.dynamoDbSvc.put(params).promise();
+  }
+
+  private handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+    return response;
+  }
+
+  private handlerError(data) {
+    return {
+      statusCode: data.statusCode || 501,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't create item!!",
+    };
+  }
 
   async main(event: APIGatewayEvent) {
     try {
-      return {
-        statusCode: 200,
-        body: "hello",
-      };
+      const data = JSON.parse(event.body ?? "");
+      const dbParams = this.prepareData(data);
+      await this.insertItem(dbParams);
+      return this.handlerSuccess(dbParams.Item);
     } catch (error) {
-      console.error(error);
-      return {
-        statusCode: 500,
-        body: "Internal Server Error",
-      };
+      console.error(error.stack);
+      return this.handlerError({ statusCode: 500 });
     }
   }
 }
